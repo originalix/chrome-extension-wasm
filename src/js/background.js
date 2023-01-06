36c8b6c2765d1c5fcf34c58d80c4a7c07660a6c1
Leon <lixiao.dev@gmail.com>
2023-01-06 10:08:28 +0800

    Use cardano-serialization-lib WASM version

--- src/js/background.js
+++ src/js/background.js
@@ -1,2 +1,16 @@
 import '../img/icon-128.png'
 import '../img/icon-34.png'
+
+console.log('Background')
+
+const Loader = async () => import('@emurgo/cardano-serialization-lib-browser')
+
+const main = async () => {
+  const CardanoWasm = await Loader()
+  console.log(CardanoWasm)
+
+  const magicProtocol = CardanoWasm.NetworkInfo.mainnet().protocol_magic()
+  console.log(magicProtocol)
+}
+
+main()  
--- src/js/popup/example.js
+++ src/js/popup/example.js
@@ -1,3 +1,17 @@
+const Loader = async () => import('@emurgo/cardano-serialization-lib-browser')
+
+const main = async () => {
+  const CardanoWasm = await Loader()
+  console.log(CardanoWasm)
+
+  console.log(CardanoWasm.NetworkInfo.mainnet)
+
+  const magicProtocol = CardanoWasm.NetworkInfo.mainnet().protocol_magic()
+  console.log(magicProtocol)
+}
+
 export default function () {
-  alert("hello! (find me on src/js/popup/example.js)");
+  // alert("hello! (find me on src/js/popup/example.js)");
+
+  main()  
 };
